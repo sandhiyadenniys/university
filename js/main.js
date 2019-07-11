2642e0aed465bb1aceab986669b8e068887764bf
Person 2 <person1@gmail.com>
2019-07-11 14:08:30 +0530

    updated on 11/07

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
-$('#menu-button').click(function() {
-  console.log("clicked");
-    $('#mobile-menu-list').toggleClass('active');
-  })
+
+// mobile navbar
+
+var menuButton = document.getElementById('menu-button');
+var mobileMenuList = document.getElementById('mobile-menu-list');
+
+menuButton.addEventListener('click' , function(){
+  mobileMenuList.classList.toggle('active');
+})
+
+
+// smooth scroll function for navigation
+
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+            $('html, body').animate({
+            scrollTop: target.offset().top - 0 + 'px'
+            }, 1000);
+            return false;
+        }
+        }
+    });
+});
